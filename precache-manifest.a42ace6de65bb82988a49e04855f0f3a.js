077954273d3d2e196546ab0f4d60f28cfc412f6f
zeeshan ali <zeeshan.ali@embibe.com>
2019-11-25 13:28:48 +0530

    Updates

---
+++ precache-manifest.a42ace6de65bb82988a49e04855f0f3a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3780b5227e4048e979c03176ffad34da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "171d551c3cad08082788",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "c82bd5462c9f83a62e91",
+    "url": "/static/js/2.2d12b51f.chunk.js"
+  },
+  {
+    "revision": "171d551c3cad08082788",
+    "url": "/static/js/main.6ee719c9.chunk.js"
+  },
+  {
+    "revision": "5bc5164faabe1b05c8a2",
+    "url": "/static/js/runtime-main.f84fddb2.js"
+  }
+]);
